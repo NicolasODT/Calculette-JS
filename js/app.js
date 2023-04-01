11e2cd47a9f83b2f2f4494d2bc4cfc4f5360c5ac
Nicolas Terwagne <123467708+NicolasODT@users.noreply.github.com>
2023-04-01 20:25:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,19 +10,20 @@ nombres.forEach((nombre) => {
     display.value += nombre.value; // value permet de récupérer la valeur de l'élément
   });
 });
+
 // si operation est cliqué, afficher la valeur de l'operation dans le display(input)
 operations.forEach((operation) => {
   operation.addEventListener("click", () => {
     display.value += operation.value; // += permet d'ajouter la valeur de l'élément à la valeur du display
   });
 });
+
 // si clear est cliqué, effacer le display(input)
 clear.addEventListener("click", () => {
   display.value = ""; // "" permet d'effacer le display
 });
+
 // si egal est cliqué, afficher le résultat de l'operation dans le display(input)
 egal.addEventListener("click", () => {
-  if (display.value) {
-    display.value = eval(display.value); // eval() permet d'évaluer une expression, c'est lui qui fais le calcul.
-  }
+  display.value = eval(display.value); // eval() permet d'évaluer une expression, c'est lui qui fais le calcul.
 });
